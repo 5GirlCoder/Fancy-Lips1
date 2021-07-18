7f0262efc609706a7c0834d77f1283f59e616853
5GirlCoder <73702929+5GirlCoder@users.noreply.github.com>
2021-07-18 13:59:24 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup() 
+{
+    canvas = createCanvas(300, 300);
+    canvas.position(455, 240);
+}
+
+function draw()
+{
+
+}
+ 
+function click()
+{
+    save('MyNewLips.png');
+}
